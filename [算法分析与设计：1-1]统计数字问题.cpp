b946a9f646305aa6860e46b64a9339cc2708013b
hongxiang2000 <48945768+hongxiang2000@users.noreply.github.com>
2019-10-16 16:35:23 +0800

    Update [算法分析与设计：1-1]统计数字问题.cpp

--- [算法分析与设计：1-1]统计数字问题.cpp
+++ [算法分析与设计：1-1]统计数字问题.cpp
@@ -1,35 +1,35 @@
 #include <iostream>
 using namespace std;
 
-void hongxiang_fun(int page) {
+void hx_fun(int page) {
     //我的函数
     //n表示页码
 
-    int hongxiang_result[10]; //我的结果
+    int hx_result[10]; //我的结果
     int i,j;
 
-    for(i=1;i<=page;i++) {
-        if(i<10) {
-            hongxiang_result[i]++;
+    for(i=1;i<=page;i++) {//从1到page逐个统计
+        if(i<10) {//个位数直接统计
+            hx_result[i]++;
         }
         else {
             j=i;
             
             while(j) {
-                hongxiang_result[j%10]++;
-                j=j/10;
+                hx_result[j%10]++;//统计最后一位数
+                j=j/10;//去掉个位
             }
         }
     }
 
-    for(i=0;i<10;i++) {
-        cout<<hongxiang_result[i]<<endl;
+    for(i=0;i<10;i++) {//输出统计结果
+        cout<<hx_result[i]<<endl;
     }
 }
 
 int main() {
     int page;
     cin>>page;
-    hongxiang_fun(page);
+    hx_fun(page);
     return 0;
 }
