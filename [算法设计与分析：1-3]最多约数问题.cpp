877f32dd71ed9ece7ef95ef53efb32e3cfaaafd7
hongxiang2000 <48945768+hongxiang2000@users.noreply.github.com>
2019-10-16 17:00:05 +0800

    Update and rename main.cpp to [算法设计与分析：1-3]最多约数问题.cpp

--- [算法设计与分析：1-3]最多约数问题.cpp
+++ [算法设计与分析：1-3]最多约数问题.cpp
@@ -2,9 +2,27 @@
 //  main.cpp
 //  [算法设计与分析：1-3]最多约数问题
 //
-//  Created by 洪湘 on 2019/10/10.
-//  Copyright © 2019 洪湘. All rights reserved.
+//  Created by hx on 2019/10/10.
+//  Copyright © 2019 hx. All rights reserved.
 //
+
+/*
+题目描述
+«问题描述：
+        正整数 x 的约数是能整除 x 的正整数。正整数 x 的约数个数记为 div(x)。例如，1，2，5，10 都是正整数10 的约数，且 div(10)=4。设 a 和 b 是 2 个正整数，a≤b，找出 a 和 b之间约数个数最多的数 x。
+«编程任务：
+        对于给定的 2 个正整数 a≤b，编程计算 a 和 b 之间约数个数最多的数。 
+输入
+第1 行有 2 个正整数 a 和 b。 
+输出
+若找到的 a 和 b 之间约数个数最多的数是 x，将 div(x)输出
+样例输入
+1 36
+样例输出
+9
+提示
+输入范围：1-1000000000
+*/
 
 #include <iostream>
 using namespace std;
